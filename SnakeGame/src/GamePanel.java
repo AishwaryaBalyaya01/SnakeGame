abac032590dc3c2f0df61f5e55ab8105b8c15ca1
AishwaryaBalyaya01 <Aishwarya.balyaya19@gmail.com>
2023-04-11 16:25:21 -0400

    Graphics added

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -1,29 +1,61 @@
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
 	
+	static final int screen_width = 600;
+	static final int screen_height = 600;
+	static final int unit_size = 50;
+	static final int game_units = (screen_width*screen_height)/unit_size;
+	static final int delay = 75;
+	final int[] x = new int[game_units];
+	final int[] y = new int[game_units];
+	int body_parts = 6;
+	int applesEaten=0;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
 	GamePanel(){
-		
+		random = new Random();
+		this.setPreferredSize(new Dimension(screen_width,screen_height));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 	}
 	
 	public void startGame() {
-		
+		newApple();
+		running=true;
+		timer = new Timer(delay,this);
+		timer.start();
 	}
 	
 	public void paintComponents(Graphics g) {
-		
-		
+		super.paintComponents(g);
+		draw(g);
 	}
 	
 	public void draw(Graphics g) {
-		
+		for(int i =0;i<screen_height/unit_size;i++) {
+			g.drawLine(i*unit_size, 0, i*unit_size, screen_height);
+			g.drawLine(0,i*unit_size, screen_width, i*unit_size);
+		}
+	}
+	
+	public void newApple() {
+		appleX = random.nextInt((int)(screen_width/unit_size))*unit_size; //to place evenly in the unit place
+		appleY = random.nextInt((int)(screen_height/unit_size))*unit_size; //to place evenly in the unit place
 		
 	}
+	
 	public void move() {
 		
 		
@@ -46,7 +78,7 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	
-	public MyKeyAdapter extends KeyAdapter{
+	public class MyKeyAdapter extends KeyAdapter{
 		@Override
 		public void keyPressed(KeyEvent e) {
 			
